8df8bae3d419c450c62a7d2858ab7615f471fb49
Wesley Williams <moto42@gmail.com>
2022-10-25 18:15:51 -0500

    move noto sans font to sans stack

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{html,js,md,njk}"],
   theme: {
@@ -19,6 +22,11 @@ module.exports = {
       'fontFamily': {
         'noto-sans': [`"Noto Sans"`, "sans-serif"]
       }
+    },
+    fontFamily: {
+      'sans': [`"Noto Sans"`, ...defaultTheme.fontFamily.sans],
+      'serif': [...defaultTheme.fontFamily.serif],
+      'mono': [...defaultTheme.fontFamily.mono]
     },
   },
   plugins: [],

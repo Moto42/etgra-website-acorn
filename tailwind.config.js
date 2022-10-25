cea843561d24942a45ec99896cd2b8af80301232
Wesley Williams <moto42@gmail.com>
2022-10-25 16:18:04 -0500

    Logo in header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,md,njk}"],
   theme: {
-    extend: {},
+    extend: {
+      'colors':{
+        'etgra': {
+          'border': {
+            'DEFAULT': '#0e7490',
+            'start': '#22d3ee',
+            'end': '#0e7490',
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
